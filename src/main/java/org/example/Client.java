fdf9b10aec68dad50c2cee59a126da0c4c7e5537
DidenkoSerhii <didenkosergey1005@gmail.com>
2024-06-19 19:55:36 +0300

    Initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import lombok.*;
+
+@Data
+public class Client {
+
+    private int id;
+    private String name;
+
+}
---
+++ src/main/java/org/example/Demo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import lombok.*;
+
+public class Demo {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        Client client = new Client();
+    }
+
+}
